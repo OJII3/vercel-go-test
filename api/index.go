c34719a11d7b47d8f0b056be5aec286ab865eb68
OJII3 <84656786+OJII3@users.noreply.github.com>
2023-05-16 15:15:16 +0900

    update

--- api/index.go
+++ api/index.go
@@ -6,7 +6,6 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// write response from public/index.html
-		http.ServeFile(w, r, "public/index.html")
+		w.Write([]byte("Hello world/ index"))
 	})
 }
